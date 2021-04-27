9108bf7ea1785f197867c68126d1dcaa1decc997
kouhei2000822 <>
2021-04-27 03:59:50 +0000

    I wrote docstring of "temp_functions.py" file.

--- temp_functions.py
+++ temp_functions.py
@@ -1,8 +1,32 @@
 def fahr_to_celsius(temp_fahrenheit):
+  """Returns a value which converted from Fahrenheit to Celsius.
+  -----Parameter-----
+  temp_fahrenheit : int or float
+    
+  -----Return-----
+  converted_temp : float  
+  """
   converted_temp=(temp_fahrenheit-32)/1.8
   return converted_temp
 
+print("48° Fahrenheit is ",fahr_to_celsius(48), "Celsius.")
+print("71° Fahrenheit is ",fahr_to_celsius(71), "Celsius.")
+
 def temp_classifier(temp_celsius):
+  """
+  Return values which grouped by Celsius value.
+  -----Parameter-----
+  temp_celsius : float
+  -----Return-----
+  0 : int
+   when temp_celsius < -2
+  1 : int
+   when -2<= temp_celsius < 2
+  2 : int
+   when 2 <= temp_celsius < 15
+  3 : int
+  when 15 <= temp_celsius
+  """
   if temp_celsius<-2:
     return 0
   elif -2<=temp_celsius<2:
@@ -11,3 +35,8 @@ def temp_classifier(temp_celsius):
     return 2
   else:
     return 3
+
+
+"""
+This file contains two functions, "fahr_to_celsius" and "temp_classifier".
+"""
